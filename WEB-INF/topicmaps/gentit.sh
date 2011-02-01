932719aa06d15909e9671670cc43ae988923ed6d
Chris Wittern <chris@mbp>
2011-02-01 18:24:47 +0900

    Tue Feb  1 18:24:47 JST 2011

--- WEB-INF/topicmaps/gentit.sh
+++ WEB-INF/topicmaps/gentit.sh
@@ -1,5 +1,5 @@
 python ~/work/py/tm-x-list-titles.py hnt-exp.ltm > /tmp/hnt-titles.txt
-python ~/work/py/tm-x-list-titles.py hkd-exp.ltm > /tmp/hkd-titles.txt
-python ~/work/py/tm-x-list-titles.py hnx-exp.ltm > /tmp/hnx-titles.txt
-python ~/work/py/tm-x-list-titles.py hmw-exp.ltm > /tmp/hmw-titles.txt
-python ~/work/py/tm-x-list-titles.py hkb-exp.ltm > /tmp/hkb-titles.txt
+python ~/work/py/tm-x-list-titles.py hkd-man.ltm > /tmp/hkd-titles.txt
+python ~/work/py/tm-x-list-titles.py hnx-man.ltm > /tmp/hnx-titles.txt
+python ~/work/py/tm-x-list-titles.py hmw-man.ltm > /tmp/hmw-titles.txt
+python ~/work/py/tm-x-list-titles.py hkb-man.ltm > /tmp/hkb-titles.txt
